15f33be4faf995f1dcad83f59f59a60aaff8e534
Yarlei Cruz <yarleicruz1998@gmail.com>
2023-11-28 10:12:52 -0300

    Update runner-chrome.js

--- runner-chrome.js
+++ runner-chrome.js
@@ -1,12 +1,15 @@
 const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImQxNmQ0YWFhLWQzMjYtNDFhMi04MGVjLTE4ZmE0NzE1ZTRhMC0xNzAxMTE1MzU2MTQwIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiNWExZGQyMzYtYjIyMi00MGYwLWJhOGEtMDdmYzcwNWQ4MzczIiwidHlwZSI6InQifQ.hHU7fxbH2EdvRAe2MxySsLe1U7x_9BNDpMTEIGFtdIA'
+
+
 cypress.run({
   browser: 'chrome'
 })
 .then((results) => {
   const args = {
-    target: process.env.TARGET_TOKEN_CHROME,
+    target: TOKEN
   }
   tesults.results(results, args);
 })
